ebb1782ba4993968585a6217fd8f60f4b88f5842
boyoung-choi <86582390+im-boyoung-choi@users.noreply.github.com>
2026-05-13 10:08:20 +0900

    주소지 업데이트

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -251,8 +251,8 @@ export const translations = {
     en: "K-DREAM, a specialized support service for international students",
   },
   "footer.address": {
-    ko: "사업장 소재지: 경상북도 포항시 남구 대이로63번길 8, 2층(대장동) | 업태: 교육서비스업 | 종목: 유학알선",
-    en: "Address: 8, Daeiro 63beon-gil, Nam-gu, Pohang-si, Gyeongsangbuk-do, 2F | Industry: Education Service | Type: Study Abroad Consulting",
+    ko: "사업장 소재지: 경북 포항시 남구 연일읍 유강길10번길 8 | 업태: 교육서비스업 | 종목: 유학알선",
+    en: "Address: 8, Yugang-gil 10beon-gil, Yeonil-eup, Nam-gu, Pohang-si, Gyeongbuk | Industry: Education Service | Type: Study Abroad Consulting",
   },
 
   // Mobile Bottom CTA
